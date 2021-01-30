5ce5e9e8cb816e24fdcd83eb4a7a3d70ce6f6229
Luis Alberto <luis.alberto199846@gmail.com>
2021-01-30 12:44:34 -0400

    Mejoras en Navbar.

--- PagonetCore/App_Start/BundleConfig.cs
+++ PagonetCore/App_Start/BundleConfig.cs
@@ -14,6 +14,10 @@ namespace PagonetCore
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
 
+            bundles.Add(new ScriptBundle("~/bundles/popper").Include(
+                        "~/Scripts/umd/popper.js",
+                        "~/Scripts/umd/popper-utils.js"));
+
             // Utilice la versión de desarrollo de Modernizr para desarrollar y obtener información. De este modo, estará
             // para la producción, use la herramienta de compilación disponible en https://modernizr.com para seleccionar solo las pruebas que necesite.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
